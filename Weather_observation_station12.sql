553ceb91afe07d555600f28ab55e5b9ca6fe8822
vvsanthosh1010 <120272460+vvsanthosh1010@users.noreply.github.com>
2023-01-12 23:10:50 +0530

    Weather Observation Station 12

---
+++ Weather_observation_station12.sql
@@ -0,0 +1,1 @@
+select distinct CITY from STATION where CITY not like 'a%' and CITY not like 'e%' and CITY not like 'i%' and CITY not like 'o%' and CITY not like 'u%' and CITY not like '%a' and CITY not like '%e' and CITY not like '%i' and CITY not like '%o' and CITY not like '%u';
